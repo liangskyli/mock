e7a94d3bb2886da2485ab887366fd502ab6ce871
liangsky <640634387@qq.com>
2022-04-23 11:15:10 +0800

    fix: sceneData type use interface replace type

--- packages/http-mock-gen/src/gen/mock-interface-type.ts
+++ packages/http-mock-gen/src/gen/mock-interface-type.ts
@@ -5,15 +5,17 @@ type IResponseData<T = any> = {
   response: T;
 };
 
+export interface ISceneDataItem<T> extends IResponseData<T> {
+  /** mock 场景数据判断,返回true时使用该场景，匹配成功后，跳出匹配 */
+  requestCase: (request: Request) => boolean;
+}
+
 export type ICustomData<T = any> = Record<
   /** http 接口路径 */
   string,
   | (IResponseData<T> & {
       /** mock 多场景响应数据 */
-      sceneData?: (IResponseData<T> & {
-        /** mock 场景数据判断,返回true时使用该场景，匹配成功后，跳出匹配 */
-        requestCase: (request: Request) => boolean;
-      })[];
+      sceneData?: ISceneDataItem<T>[];
     })
   | undefined
 >;
