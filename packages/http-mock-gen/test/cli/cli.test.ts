8dc3ef75597812bfa196fe3e8332ff61ad02497b
liangsky <640634387@qq.com>
2023-05-09 11:24:24 +0800

    test(http-mock-gen): before cli test, remove custom-data

--- packages/http-mock-gen/test/cli/cli.test.ts
+++ packages/http-mock-gen/test/cli/cli.test.ts
@@ -1,12 +1,17 @@
 import { winPath } from '@liangskyli/utils';
 import { execa } from 'execa';
+import fs from 'fs-extra';
 import { URL } from 'node:url';
+import path from 'path';
 import { describe, expect, test } from 'vitest';
 
 const cwd = new URL('../../', import.meta.url);
 const cmd = 'node';
 describe('CLI', () => {
   test('CLI test', async () => {
+    await fs.remove(
+      path.join(__dirname, '../all-gen-dirs/gen-mock-cli/mock/custom-data'),
+    );
     const { stdout } = await execa(
       cmd,
       ['./bin/index.js', '-c', './test/cli/mock.config.cli.ts'],
