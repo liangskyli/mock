baa90287eb0604eaac132cac4e30a2bbdbed215c
liangsky <640634387@qq.com>
2024-02-26 20:43:49 +0800

    chore: upgrade dependencies

--- packages/utils/vitest.config.ts
+++ packages/utils/vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
-    include: ['./**/*.test.{ts,js}'],
+    include: ['**/*.test.{ts,js}'],
     environment: 'node',
     testTimeout: 1000 * 30,
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,js}'],
+    },
   },
 });
